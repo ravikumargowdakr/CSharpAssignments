e6e95bf877be6b121d90050c054f73e6e2161203
RAVI KUMAR KR <77578890+ravikumargowdakr@users.noreply.github.com>
2023-04-14 07:16:43 +0530

    Add files via upload

---
+++ SemaphoreThreads.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Assignment
+{
+    internal class Program
+    {
+
+        private static Semaphore _pool;
+
+        private static int _padding;
+        static void Main(string[] args)
+        {
+            _pool = new Semaphore(initialCount: 0, maximumCount: 3);
+            for (int i = 1; i <= 5; i++)
+            {
+                Thread th = new Thread(new ParameterizedThreadStart(Worker));
+                th.Start(i);
+            }
+            Thread.Sleep(500);
+            Console.WriteLine("Main thread calls Release(3).");
+            _pool.Release(releaseCount: 3);
+
+            Console.WriteLine("Main thread exits.");
+        }
+        private static void Worker(object num)
+        {
+            Console.WriteLine("Thread {0} begins " +
+                "and waits for the semaphore.", num);
+            _pool.WaitOne();
+
+            int padding = Interlocked.Add(ref _padding, 100);
+
+            Console.WriteLine("Thread {0} enters the semaphore.", num);
+
+ 
+            Thread.Sleep(1000 + padding);
+
+            Console.WriteLine("Thread {0} releases the semaphore.", num);
+            Console.WriteLine("Thread {0} previous semaphore count: {1}",
+                num, _pool.Release());
+        }
+    }
+}
---
+++ SoapSerialisation.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Assignment
+{
+    // Define a class to be serialized
+    [Serializable]
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string EmpName { get; set; }
+        
+    }
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the Person class
+            Employee emp = new Employee {  Id = 121 , EmpName = "RAVI" };
+
+            // Serialize the object to a SOAP file
+            XmlSerializer serializer = new XmlSerializer(typeof(Employee));
+            //using (FileStream stream = new FileStream("Employee.soap", FileMode.Create))
+            //{
+            //    serializer.Serialize(stream, emp);
+            //}
+
+            //Deserialize the object from the SOAP file
+            using (FileStream stream = new FileStream("Employee.soap", FileMode.Open))
+            {
+                Employee deserializedPerson = (Employee)serializer.Deserialize(stream);
+                Console.WriteLine($" Id: {deserializedPerson.Id}, EmpName: {deserializedPerson.EmpName}");
+            }
+        }
+    }
+}
